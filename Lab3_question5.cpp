7dc8720febb6f03c32bdb10335c612e43f039dc7
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-09-04 14:51:10 +0530

    check whether a number is even or odd

---
+++ Lab3_question5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num,i;
+cout <<"Enter a positive integer\n";
+cin >> num;
+i=num%2;
+if(i==0)
+cout <<"Number is even\n";
+else 
+cout <<"Number is odd\n";
+return 0;
+}
