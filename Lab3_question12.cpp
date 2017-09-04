d51da8b9563e4efe85c333597d7b4211c07703e0
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-09-04 15:41:16 +0530

    Update Lab3_question12.cpp

--- Lab3_question12.cpp
+++ Lab3_question12.cpp
@@ -5,10 +5,10 @@ int main() {
         cout <<"Enter the month number\n";
         cin >>m;
         if (m==1||m==3||m==5||m==7||m==8||m==10||m==12)
-        cout <<"The month has thirty one days";
+        cout <<"The month has thirty one days\n";
         else if (m==2)
-        cout <<"The month has twenty eight days";
+        cout <<"The month has twenty eight days\n";
         else 
-        cout <<"The month has thirty days";
+        cout <<"The month has thirty days\n";
     	return 0;
     }
