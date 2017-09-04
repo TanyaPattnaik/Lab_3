bcf25ca402a4f02f4524ca8d55a352bdeac1db11
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-09-04 15:37:00 +0530

    Create Lab3_question11.cpp

---
+++ Lab3_question11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num;
+cout <<"Enter number of the day in a week\n";
+cin >> num;
+    	if (num == 1)
+    	cout <<"Sunday\n";
+    	else if (num == 2)
+    	cout <<"Monday\n";
+    	else if (num == 3)
+    	cout <<"Tuesday\n";
+    	else if (num == 4)
+    	cout <<"Wednesday\n";
+    	else if (num == 5)
+    	cout <<"Thursday\n";
+    	else if (num == 6)
+    	cout <<"Friday\n";
+    	else if (num == 7)
+    	cout <<"Saturday\n";
+        else 
+        cout <<"Invalid choice\n";
+    	return 0;
+    }
