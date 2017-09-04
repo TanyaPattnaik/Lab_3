1e35e3179b95a1bba2561df561a2794aaa8b7c47
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-09-04 15:21:06 +0530

    Update Lab3_question08.cpp

--- Lab3_question08.cpp
+++ Lab3_question08.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
 	char a;
-	cout <<"Enter a character\n";
+	cout <<"Enter an alphabet\n";
 	cin >>a;
 	if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
 	cout <<"The alphabet is a vowel\n";
