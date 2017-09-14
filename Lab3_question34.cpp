bf670ab7ea4bf7626ab7204dbbdf0a00eb807ed5
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-09-14 22:32:55 +0530

    Create Lab3_question34.cpp

---
+++ Lab3_question34.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n,newno,fd,p,ld,d=0;
+	cout<<"Enter a number\n";
+	cin>>n;
+	int num=n;
+	while(n>0){
+	n=n/10;
+	d++;
+	}
+	p=pow(10,(d-1));
+	fd=(int)num/p;
+	ld=num%10;
+	num=num-(fd*p);
+	num=num/10;
+	newno=ld*p+num*10+fd;
+    cout<<"\nNumber after swapping first and last digit: "<<newno;
+
+    return 0;
+}
